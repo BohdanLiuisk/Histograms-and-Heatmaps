530dcae27c0a7572e2f2f4e39e6bb844c0aa176d
Bogdan Liusik <bodialiusik@gmail.com>
2021-02-17 18:04:39 +0200

    Add files via upload

---
+++ HistogramsHeatMap/HistogramTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HistogramsHeatMap
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var minDay = 1;
+            var maxDay = int.MinValue;
+            foreach (var day in names)
+                maxDay = Math.Max(maxDay, day.BirthDate.Day);
+            var days = new string[maxDay - minDay + 1];
+
+            for (var i = 0; i < days.Length; i++)
+            {
+                days[i] = (i + minDay).ToString();
+            }
+
+            var birthCounts = new double[maxDay - minDay + 1];
+
+            foreach (var day in names)
+            {
+                if (day.Name == name && day.BirthDate.Day > 1)
+                    birthCounts[day.BirthDate.Day - minDay]++;
+            }
+
+            return new HistogramData(String.Format("Рождаемость людей с именем '{0}'", UppercaseFirst(name)), days, birthCounts);
+        }
+
+        private static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            char[] a = s.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+    }
+}
---
+++ HistogramsHeatMap/NameData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace HistogramsHeatMap
+{
+    public class NameData
+    {
+        public DateTime BirthDate;
+        public string Name;
+
+        public NameData(int year, int month, int day, string name) : this(new DateTime(year, month, day), name) { }
+
+        public NameData(DateTime birthDate, string name)
+        {
+            BirthDate = birthDate;
+            Name = name;
+        }
+
+        public static NameData ParseFrom(string textLine)
+        {
+            var parts = textLine.Split('\t');
+            const string format = "dd.MM.yyyy";
+            var date = DateTime.ParseExact(parts[0], format, CultureInfo.InvariantCulture);
+            return new NameData(date, parts[1]);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}    {1}", BirthDate.ToString("dd.MM.yyyy"), Name);
+        }
+    }
+}
